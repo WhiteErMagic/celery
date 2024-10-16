fb9bdbbb3f47704f3af8a5338279800cf87ece76
Boris <kabv@mail.ru>
2024-10-16 12:35:57 +0600

    Commit4

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from flask.views import MethodView
 from upscale import upscale
 
 from tasks import task_upscale
+from flask import send_file
 
 
 app_name = 'app'
@@ -27,13 +28,14 @@ celery.Task = ContextTask
 
 
 @celery.task()
-def task_upscale():
-    task1 = task_upscale()
+def task_upscale(image_path, output_path):
+    upscale(image_path, output_path)
+    return output_path
 
 
-@app.route('/download/<string:name>')
+@app.route('/processed/<string:name>')
 def download_file(name):
-    return jsonify({'file': os.path('files', f'{name}')})
+    return send_file(name)
 
 
 class Photo(MethodView):
@@ -43,26 +45,32 @@ class Photo(MethodView):
         return jsonify({'status': task.status, 'result': task.result})
 
     def post(self):
-        image_path = ('image_1')
-        print(image_path)
-        image_path = self.save_image(image_path)
-        task = task_upscale(*image_path)
+        image_path_in = ('image_1')
+        image_path = self.save_image(image_path_in)
+        output_path = self.upscale_path(image_path_in)
+        task_upscale(image_path, output_path)
         return jsonify(
-            {'task_id': task.id}
+            {'output_path': output_path}
         )
+
+    def upscale_path(self, field):
+        image = request.files.get(field)
+        extantion = image.filename.split('.')[-1]
+        path = os.path.join('files', f'{uuid.uuid4()}.{extantion}')
+        return path
 
     def save_image(self, field):
         image = request.files.get(field)
         extantion = image.filename.split('.')[-1]
-        path = os.path('files', f'{uuid.uuid4()}.{extantion}')
+        path = os.path.join('files', f'{uuid.uuid4()}.{extantion}')
         image.save(path)
         return path
 
 
 photo_view = Photo.as_view('photo')
-app.add_url_rule('/download/<string:name>', view_func=photo_view, methods=['GET'])
+app.add_url_rule('/processed/<string:name>', view_func=download_file, methods=['GET'])
 app.add_url_rule('/photo/<string:id>', view_func=photo_view, methods=['GET'])
-app.add_url_rule('/photo/', view_func=photo_view, methods=['POST'])
+app.add_url_rule('/upscale/', view_func=photo_view, methods=['POST'])
 
 
 if __name__ == '__main__':
